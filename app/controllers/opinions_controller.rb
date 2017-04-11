7303258559aa4a607a80dc8c4c09d871f3b3b04d
Thibault Pedelhez <thi.pedelhez@gmail.com>
2017-04-11 14:54:45 +0200

    ok

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -65,7 +65,7 @@ class OpinionsController < ApplicationController
 
     respond_to do |format|
 
-        format.html { redirect_to new_card_opinion_path(card_id: ([*6..117] - a).sample)   }
+        format.html { redirect_to new_card_opinion_path(card_id: ([*6..117]).sample)   }
         format.json { render :show, status: :ok, location: @opinion }
 
     end
